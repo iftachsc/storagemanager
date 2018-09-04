39daa4deda1094e7dfae77cc3868c24d325a32d3
Iftach Schonbaum <iftachsc@innercloud.io>
2018-09-04 17:14:27 +0300

    updated zadara tests

--- zadara/test/zadara_test.go
+++ zadara/test/zadara_test.go
@@ -3,7 +3,6 @@ package zadara_test
 import (
 	"fmt"
 	"net/http"
-	"strings"
 	"testing"
 
 	"github.com/iftachsc/storagemanager/zadara"
@@ -26,17 +25,19 @@ const (
 	VpsaToken    = "token"
 )
 
+func getClient(apiPath string) (*ZadaraClient,string) {
+	return zadara.NewClient(zadara.ZadaraCredentials{ VpsaHost, VpsaUser, 
+		VpsaPassword, VpsaToken }
+	), fmt.Sprintf("http://%s/%s", VpsaHost, )
+}
+
 //API Tests
 func TestGetVolumesOK(t *testing.T) {
 
-	c := zadara.NewClient(zadara.ZadaraCredentials{VpsaHost, VpsaUser, VpsaPassword, VpsaToken})
-	url := fmt.Sprintf("http://%s/%s", VpsaHost, zadara.VolumesPath)
-	println(fmt.Sprintf("Mocking json on %s", url))
-	//initHttpMock(http.MethodGet, url, RootVolumeResponseJson)
+	c, url := getClientAndApiUrl(zadara.VolumesPath)
 
 	httpmock.Activate()
 
-	//url e.g. "https://api.mybiz.com/articles.json"
 	httpmock.RegisterResponder(http.MethodGet, url,
 		httpmock.NewStringResponder(200, RootVolumeResponseJson))
 
@@ -47,33 +48,8 @@ func TestGetVolumesOK(t *testing.T) {
 		t.Errorf(err.Error())
 	}
 
-	if len(vols) != 2 {
-		t.Errorf("Expected 2 volume but got %d", len(vols))
-	}
-
+	assert.Equal(t, 2, len(vols), "volume count should be equal")
 	assert.Equal(t, MockVolume1.Name, vols[0].Name, "volume name should be equal")
 	assert.Equal(t, MockVolume1.PoolName, vols[0].PoolName, "pool name should be equal")
 	assert.Equal(t, MockVolume1.Encryption, vols[0].Encryption, "vol names should be equal")
-
-}
-
-func Get(apiPath string, host string) error {
-	url := fmt.Sprintf("http://%s/%s", host, apiPath)
-	payload := fmt.Sprintf(`
-			{
-				"meta": {
-					"lifeMeaning": "h"
-				},
-				"data": {
-					" “message”: "j"
-				}
-			}`)
-	resp, err := http.Post(url, "application/json", strings.NewReader(payload))
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("nsqd didn’t respond 200 OK: %s", resp.Status)
-	}
-	return nil
 }
